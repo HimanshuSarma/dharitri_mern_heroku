39c6b909a0fb1fe6d6ed8ef642d9af47efb04d2e
Himanshu Sharma <sweetiehimanshu@gmail.com>
2022-07-05 12:36:57 -0700

    commit

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.use(cookieParser());
 
 __dirname = path.resolve();
 app.use('*', (req, res) => {
-        res.sendFile('./routes/User-routes');
+        res.sendFile('./routes/User-routes.js');
     })
     // app.use(express.static(path.join(__dirname + '/backend/data/productsImgs/airpods.jpg')));
     // app.use('*', (req, res) => {
