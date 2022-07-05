80602ff24ca5b5f3da361d939fcf882708115a38
Himanshu Sharma <sweetiehimanshu@gmail.com>
2022-07-05 11:54:57 -0700

    commit

--- server.js
+++ server.js
@@ -64,11 +64,11 @@ app.use('/', ProductRoutes.router);
 
 app.use('/', CartRoutes.router);
 
-__dirname = path.resolve();
-app.use(express.static(path.join(__dirname, './frontend/build')));
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-})
+// __dirname = path.resolve();
+// app.use(express.static(path.join(__dirname, './frontend/build')));
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+// })
 
 try {
     mongoose.connect(process.env.MONGO_DB_CONNECTION_URI, () => {
