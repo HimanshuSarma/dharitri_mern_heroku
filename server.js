6aa96c3cb001ac76049532deae144bfd1b64a1b2
Himanshu Sharma <sweetiehimanshu@gmail.com>
2022-07-05 11:49:43 -0700

    commit

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(cookieParser());
 
 __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.use(express.static(path.join(__dirname, './frontend/build')));
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
 })
