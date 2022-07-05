50daedf4a97b753ea10a69e475b40f047a21430e
Himanshu Sharma <sweetiehimanshu@gmail.com>
2022-07-05 12:05:36 -0700

    commit

--- server.js
+++ server.js
@@ -65,9 +65,9 @@ app.use('/', ProductRoutes.router);
 app.use('/', userTokenVerification.router, CartRoutes.router);
 
 __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, './frontend/build')));
+app.use(express.static(path.join(__dirname + '/frontend/build')));
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './frontend/build/index.html'));
+    res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
 })
 
 try {
