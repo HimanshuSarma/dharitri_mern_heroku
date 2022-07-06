d201da54755fc91f16f4675536e47b091954c02a
Himanshu Sharma <sweetiehimanshu@gmail.com>
2022-07-05 23:31:32 -0700

    commit

--- frontend/src/Data/config.js
+++ frontend/src/Data/config.js
@@ -1,4 +1,4 @@
 const domain = document.domain;
 console.log(domain);
 
-export const base_url = (domain === 'localhost') ? domain : 'https://dharitri.herokuapp.com';
+export const base_url = (domain === 'localhost:3000') ? domain : 'https://dharitri.herokuapp.com';
