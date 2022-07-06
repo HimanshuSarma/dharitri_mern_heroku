40b9967e67a3c2b76240415bc66a3bc72f6b911d
Himanshu Sharma <sweetiehimanshu@gmail.com>
2022-07-05 23:21:48 -0700

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,7 @@ import {checkUserLogin} from './redux/ActionCreators/userActions';
 import './App.css';
 
 function App() {
-  console.log(base_url);
+  console.log(base_url, document.domain());
 
   const dispatch = useDispatch();
 
--- frontend/src/Data/config.js
+++ frontend/src/Data/config.js
@@ -1,1 +1,4 @@
+const domain = document.domain();
+console.log(domain);
+
 export const base_url = 'https://dharitri.herokuapp.com';
